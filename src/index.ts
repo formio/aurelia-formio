abf1f356e20aa337ce8f671a53bde4a489862536
Randall Knutson <randall@form.io>
2018-07-02 16:00:09 -0500

    Reformat.

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { FrameworkConfiguration } from 'aurelia-framework';
 import { PLATFORM } from 'aurelia-pal';
+
 export function configure(aurelia: FrameworkConfiguration) {
-    aurelia.globalResources([
-        PLATFORM.moduleName('./formio'),
-        PLATFORM.moduleName('./builder')
-    ]);
+  aurelia.globalResources([
+    PLATFORM.moduleName('./formio'),
+    PLATFORM.moduleName('./builder')
+  ]);
 }
