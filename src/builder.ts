8f4d8914b71fb939ab7c04b126ec93a99efe3f5a
Randall Knutson <randall@form.io>
2018-07-02 15:59:38 -0500

    Reformat again.

--- src/builder.ts
+++ src/builder.ts
@@ -1,60 +1,61 @@
 import { inject } from 'aurelia-dependency-injection';
 import { bindable } from 'aurelia-framework';
 import { customElement } from 'aurelia-templating';
+
 const FormBuilder = require('formiojs').FormBuilder;
 
 @customElement('form-builder')
 @inject(Element)
 export class FormRenderer {
-    public instance: any;
-    private builder: Element;
-    private element: Element;
+  public instance: any;
+  private builder: Element;
+  private element: Element;
 
-    @bindable
-    private form: object = {};
+  @bindable
+  private form: object = {};
 
-    @bindable
-    private options: any = {};
+  @bindable
+  private options: any = {};
 
-    constructor(element: Element) {
-        this.element = element;
+  constructor(element: Element) {
+    this.element = element;
+  }
+
+  emit(name: string, data: object) {
+    this.element.dispatchEvent(new CustomEvent(name, {
+      bubbles: true,
+      detail: {
+        component: data,
+        schema: this.instance.schema
+      }
+    }));
+  }
+
+  renderBuilder() {
+    if (this.builder) {
+      (new FormBuilder(this.builder, this.form, this.options))
+        .render()
+        .then((instance: any) => {
+          this.instance = instance;
+          this.instance.off('deleteComponent');
+          this.instance.off('updateComponent');
+          this.instance.off('saveComponent');
+          this.instance.on('deleteComponent', (event: any) => this.emit('change', event));
+          this.instance.on('updateComponent', (event: any) => this.emit('change', event));
+          this.instance.on('saveComponent', (event: any) => this.emit('change', event));
+        });
     }
+  }
 
-    emit(name: string, data: object) {
-        this.element.dispatchEvent(new CustomEvent(name, {
-            bubbles: true,
-            detail: {
-                component: data,
-                schema: this.instance.schema
-            }
-        }));
-    }
+  attached() {
+    this.renderBuilder();
+  }
 
-    renderBuilder() {
-        if (this.builder) {
-            (new FormBuilder(this.builder, this.form, this.options))
-                .render()
-                .then((instance: any) => {
-                    this.instance = instance;
-                    this.instance.off('deleteComponent');
-                    this.instance.off('updateComponent');
-                    this.instance.off('saveComponent');
-                    this.instance.on('deleteComponent', (event: any) => this.emit('change', event));
-                    this.instance.on('updateComponent', (event: any) => this.emit('change', event));
-                    this.instance.on('saveComponent', (event: any) => this.emit('change', event));
-                });
-        }
-    }
+  optionsChanged() {
+    this.renderBuilder();
+  }
 
-    attached() {
-        this.renderBuilder();
-    }
-
-    optionsChanged() {
-        this.renderBuilder();
-    }
-
-    formChanged() {
-        this.renderBuilder();
-    }
+  formChanged() {
+    this.renderBuilder();
+  }
 }
